62841349db1f1703dffbb5f5ecbb7142de022553
deguzmankarladrian <127069522+deguzmankarladrian@users.noreply.github.com>
2023-03-07 15:53:20 +0800

    Update video.py

--- video.py
+++ video.py
@@ -4,6 +4,6 @@ import cv2
 
 model = YOLO('best.pt')
 
-results = model.predict(source='DJI_20210929115851_0092_SUPR.JPG', show=True)
+results = model.predict(source='0', show=True, conf=0.7)
 
 print(results)
